80d829923bb6b0f6812943b66a4147db934c913c
Raul Sulaimanov <raul.sulaimanov@gmail.com>
2019-02-05 15:31:03 +0100

    Add files via upload

---
+++ magicEightBall.js
@@ -0,0 +1,31 @@
+const userQuestion = "Will I pass my Calculus exam, tomorrow?";
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Cannot predict now';
+    break;
+  case 3:
+    eightBall = 'Don\'t count on it';
+    break;
+  case 4:
+    eightBall = 'My sources say no';
+    break;
+  case 5:
+    eightBall = 'Outlook not so good';
+    break;
+  case 6:
+    eightBall = 'Signs point to yes';
+    break;
+  case 7:
+    eightBall = 'You are in grave danger';
+    break;
+}
+console.log('The user asked: ' + userQuestion);
+console.log('The eight ball answered: ' + eightBall);
